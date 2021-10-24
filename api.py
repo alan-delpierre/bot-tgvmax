530412e47e6296d0148112129c151febc4be8fa6
alan-delpierre <delpierrealan@gmail.com>
2021-10-24 23:08:10 +0200

    pas mal

--- api.py
+++ api.py
@@ -1,63 +1,51 @@
 import requests, sys, json
 
 cities_list = ["NANTES", 'MASSY TGV', "PARIS"]
-URL_TGVMAX = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&facet=od_happy_card"
 
-def main():
-    test_connect(URL_TGVMAX)
 
 #Test de connexion à l'endpoint de l'api
 def test_connect(url):
-    response = requests.get(URL_TGVMAX)
+    response = requests.get(url)
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         return "Error: " + str(e)
-    pull_datas(URL_TGVMAX)
+    
 
 #Récupération des données en json selon les villes choisies par l'utilisateur
-def pull_datas(url):
+def pull_datas(origine, destination, date):
+    URL_TGVMAX = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&facet=od_happy_card"
+    test_connect(URL_TGVMAX)
 
-    print("Villes disponibles : {0}".format(cities_list))
-
-    while True :
-        try:
-            origine_city = input("Ville de départ : ").upper()
-            if origine_city in cities_list :
-                break
-        except origine_city not in cities_list:
-            continue
-
-        
-    while True :
-        try:
-            destination_city = input("Ville d'arrivé : ").upper()
-            if destination_city in cities_list :
-                break
-        except destination_city not in cities_list:
-            continue
+    origine = origine.upper()
+    destination = destination.upper()
     
     #On va faire correspondre l'entrée "paris" avec "paris (intramuros)" qui est utilisé dans l'API 
-    cities_data = {'origine': origine_city, 'destination': destination_city}
+    cities_data = {'origine': origine, 'destination': destination}
     for k, v in cities_data.items():
         if v == "PARIS":
             cities_data[k] = "PARIS (intramuros)"
 
-    datas = {'refine.origine' : cities_data['origine'], 'refine.destination' : cities_data['destination'], 'refine.date': '2021-11-05'}
-    response = requests.get(url, params=datas)
-    contenu = response.json()
+    datas = {'refine.origine' : cities_data['origine'], 'refine.destination' : cities_data['destination'], 'refine.date': date}
+    response = requests.get(URL_TGVMAX, params=datas)
+    data_dict = response.json()
 
     with open("data.json", "w") as f:
-        json.dump(contenu, f, indent=4, ensure_ascii=False)
+        json.dump(data_dict, f, indent=4, ensure_ascii=False)
         f.close()
 
-    read_datas()
+    responses_list = []
+    trains_list = []
 
-def read_datas():
-    with open("data.json", "r") as f :
-        data_dict = json.load(f)
-        f.close()
-    print(data_dict['records'])
+    for x in data_dict['records'] :
+        responses_list.append(x)
+    
+    for i in range(len(responses_list) - 1):
+        fields = data_dict['records'][i]
+        datas_dict = fields['fields']
+        trains_details = {"depart" : datas_dict["heure_depart"], "arrive":datas_dict["heure_arrivee"], "train":datas_dict["train_no"]}
+        trains_list.append(trains_details)
+    print(trains_list)
+    return trains_list
 
-if __name__ == "__main__" :
-    read_datas()
+
--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-import discord, logging, os
+import logging, os
 from dotenv import load_dotenv
+from api import test_connect, pull_datas
+from discord.ext import commands
 
 load_dotenv()
 
@@ -12,18 +14,38 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello !')
+@bot.command(name='del')
+async def delete(ctx, number_of_messages: int):
+    messages = await ctx.channel.history(limit=number_of_messages + 1).flatten()
 
-client.run(TOKEN)
+    for each_message in messages :
+        await each_message.delete()
+
+
+@bot.command(name='get')
+async def get(ctx, message):
+    infos = message.split('/')
+    if len(infos) != 3 :
+        await ctx.send("Le format de la demande doit être '!get VILLE1/VILLE2/YYYY-MM-DD'")
+    origine_city = infos[0]
+    destination_city = infos[1]
+    date = infos[2]
+    trains_list = pull_datas(origine_city, destination_city, date)
+    if len(trains_list) == 0 :
+        message = "Il y a n'y a pas de train disponible à cette date."
+    else :
+        message = "Il y a {0} trains disponibles \n".format(len(trains_list))
+        for i in range(len(trains_list)):
+            message2 = "Train numéro {0} : \nHeure de départ : {1}\n Heure d'arrivée : {2}\n\n".format(trains_list[i]['train'], trains_list[i]['depart'], trains_list[i]['arrive'])
+            message += message2
+
+    await ctx.send(message)
+
+bot.run(TOKEN)
